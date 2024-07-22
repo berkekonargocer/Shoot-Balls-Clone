86a2b694b301ab3fa73482cf87ba4850fcdf7820
Berke Konargöçer <berke.konargocer@gmail.com>
2024-07-22 09:33:02 +0300

    AutoForwardMovement.cs AudioManager.cs GameManager.cs and ScoreManager.cs Created and Implemented

---
+++ Assets/Scripts/AutoForwardMovement.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+//using UnityEngine.InputSystem;
+
+public enum State
+{
+    IDLE,
+    MOVING
+}
+
+[DisallowMultipleComponent]
+[RequireComponent(typeof(Rigidbody))]
+//[RequireComponent(typeof(PlayerInput))]
+public class AutoForwardMovement : MonoBehaviour
+{
+    //[field: SerializeField] public float HorizontalMovementSpeed { get; private set; } = 6.0f;
+    [field: SerializeField] public float MovementSpeed { get; private set; } = 32.5f;
+
+    //PlayerInput _playerInput;
+    Rigidbody _objectRigidbody;
+
+    //Vector2 _moveDirection;
+
+    const float MAX_X_POSITION = 4.85f;
+    const float VERTICAL_MOVEMENT_SPEED_MULTIPLIER = 50.0f;
+    //const float HORIZONTAL_MOVEMENT_SPEED_MULTIPLIER = 20.0f;
+
+    public State CharState { get; private set; } = State.IDLE;
+
+
+    void OnEnable() {
+        GameManager.Instance.OnWinGame += StopMovement;
+        GameManager.Instance.OnLoseGame += StopMovement;
+        GameManager.Instance.OnStartGame += StartMovement;
+    }
+
+    void OnDisable() {
+        GameManager.Instance.OnWinGame -= StopMovement;
+        GameManager.Instance.OnLoseGame -= StopMovement;
+        GameManager.Instance.OnStartGame -= StartMovement;
+    }
+
+    void Awake() {
+        _objectRigidbody = GetComponent<Rigidbody>();
+        //_playerInput = GetComponent<PlayerInput>();
+    }
+
+    void Update() {
+        switch (CharState)
+        {
+            case State.IDLE:
+                break;
+            case State.MOVING:
+                ApplyMovement();
+                break;
+
+        }
+    }
+
+
+    public void IncrementVerticalMovementSpeed(float speedToAddUp) {
+        MovementSpeed += speedToAddUp;
+    }
+
+    public void SetVerticalMovementSpeed(float speed) {
+        MovementSpeed = speed;
+    }
+
+
+    Vector3 GetMovementDirection() {
+        //if (_playerInput.actions["LeftClick"].ReadValue<float>() == 0)
+        //{
+        //    _moveDirection = Vector3.zero;
+        //}
+        //else
+        //{
+        //    _moveDirection = _playerInput.actions["Move"].ReadValue<Vector2>();
+        //}
+
+        Vector3 moveDirection = new Vector3(0, 0, MovementSpeed * VERTICAL_MOVEMENT_SPEED_MULTIPLIER) * Time.deltaTime;
+
+        return moveDirection;
+    }
+
+    void ApplyMovement() {
+        _objectRigidbody.AddForce(GetMovementDirection(), ForceMode.Force);
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -MAX_X_POSITION, MAX_X_POSITION), transform.position.y, transform.position.z);
+    }
+
+    void StartMovement() {
+        CharState = State.MOVING;
+    }
+
+    void StopMovement(int score) {
+        CharState = State.IDLE;
+        _objectRigidbody.velocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+using NOJUMPO;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[DisallowMultipleComponent]
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [field: SerializeField] public AudioClip BackgroundMusic { get; private set; }
+    [field: SerializeField] public AudioClip WinGameSFX { get; private set; }
+    [field: SerializeField] public AudioClip LoseGameSFX { get; private set; }
+
+    public event Action OnStartGame;
+    public event Action<int> OnLoseGame;
+    public event Action<int> OnWinGame;
+
+    bool isGameOver = false;
+
+
+    void Awake() {
+        InitializeSingleton();
+    }
+
+    void OnEnable() {
+
+    }
+
+
+    public void StartGame() {
+        OnStartGame?.Invoke();
+        AudioManager.Instance.PlayMusic(BackgroundMusic);
+    }
+
+    public void WinGame() {
+        AudioManager.Instance.StopMusic();
+        AudioManager.Instance.PlaySFX(WinGameSFX);
+        OnWinGame?.Invoke(ScoreManager.Instance.Score);
+    }
+
+    public void LoseGame() {
+        if (isGameOver)
+            return;
+
+        isGameOver = true;
+        AudioManager.Instance.StopMusic();
+        AudioManager.Instance.PlaySFX(LoseGameSFX);
+        OnLoseGame?.Invoke(ScoreManager.Instance.Score);
+    }
+
+    public void RestartLevel() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+
+    void InitializeSingleton() {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
