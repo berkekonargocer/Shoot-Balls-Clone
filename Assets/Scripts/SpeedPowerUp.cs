a78ec94d4563477bb879f2bd016004c92b59d67c
Berke Konargöçer <berke.konargocer@gmail.com>
2024-07-23 15:13:00 +0300

    SpeedPowerUp.cs ITargetable interface update

--- Assets/Scripts/SpeedPowerUp.cs
+++ Assets/Scripts/SpeedPowerUp.cs
@@ -7,7 +7,10 @@ public class SpeedPowerUp : MonoBehaviour, ITargetable
     [SerializeField] TextMeshPro speedAmountText;
     [SerializeField] float powerUpAmount = 2.0f;
 
+    public float ArcAmount { get; private set; } = 1.0f;
+    public bool IsMoving { get; private set; } = false;
     public bool IsTargeted { get; private set; }
+
 
     public Vector3 OnTargeted(Transform player) {
         return targetTransform.position;
