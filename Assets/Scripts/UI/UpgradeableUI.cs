82a3a3698e28cf5fe408f77e47354f7f03b7b433
Berke Konargöçer <berke.konargocer@gmail.com>
2024-07-27 15:34:40 +0300

    UpgradeableUI.cs Created

---
+++ Assets/Scripts/UI/UpgradeableUI.cs
@@ -0,0 +1,25 @@
+using TMPro;
+using UnityEngine;
+
+public class UpgradeableUI : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI ballsText;
+    [SerializeField] TextMeshProUGUI ballUpgradeAmountText;
+    [SerializeField] Sprite ballSprite;
+
+    [Space]
+
+    [SerializeField] TextMeshProUGUI evolveText;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
