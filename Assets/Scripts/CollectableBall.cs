9586962aa08a804bd971ea80b111d723036ac0f6
Berke Konargöçer <81126435+berkekonargocer@users.noreply.github.com>
2024-07-29 16:07:57 +0300

    CollectableBall.cs Code Line Organization

--- Assets/Scripts/CollectableBall.cs
+++ Assets/Scripts/CollectableBall.cs
@@ -23,6 +23,28 @@ public class CollectableBall : MonoBehaviour, ITargetable
         pointAmountText.text = PointAmount.ToString();
     }
 
+    void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player"))
+        {
+            gameObject.layer = defaultLayer;
+            MoveInArc(transform, new Vector3(-3.5f, transform.position.y, transform.position.z + 2.5f), 0.5f, 1.0f);
+        }
+
+        if (other.CompareTag("BallStacker"))
+        {
+            other.transform.gameObject.GetComponent<BallStacker>().AddToStack(this);
+            gameObject.GetComponent<Collider>().enabled = false;
+        }
+    }
+    
+    void OnTriggerStay(Collider other) {
+        if (other.CompareTag("BallWay"))
+        {
+            transform.position += Vector3.forward * 0.25f;
+        }
+    }
+
+    
     public Vector3 OnTargeted(Transform player) {
         if (targetPosition == null)
             return Vector3.forward;
@@ -37,26 +59,6 @@ public class CollectableBall : MonoBehaviour, ITargetable
         Destroy(ball.gameObject);
     }
 
-    void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player"))
-        {
-            gameObject.layer = defaultLayer;
-            MoveInArc(transform, new Vector3(-3.5f, transform.position.y, transform.position.z + 2.5f), 0.5f, 1.0f);
-        }
-
-        if (other.CompareTag("BallStacker"))
-        {
-            other.transform.gameObject.GetComponent<BallStacker>().AddToStack(this);
-            gameObject.GetComponent<Collider>().enabled = false;
-        }
-    }
-
-    void OnTriggerStay(Collider other) {
-        if (other.CompareTag("BallWay"))
-        {
-            transform.position += Vector3.forward * 0.25f;
-        }
-    }
 
     void MoveInArc(Transform objectToMove, Vector3 targetPosition, float duration, float arcHeight) {
         Vector3 midPoint = (objectToMove.position + targetPosition) / 2 + Vector3.up * arcHeight;
